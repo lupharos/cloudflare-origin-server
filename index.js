2a197131e80acb41f8e1e223343ad45a9a18dd5b
Lupharos <websenseturkey@gmail.com>
2025-07-26 23:08:12 +0300

    Initial commit for Cloudflare Origin Server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json(req.headers);  // gelen header'ları JSON olarak döner
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
