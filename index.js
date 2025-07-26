604ba567a9bd299022bbe6fc82e9e30eb0fb41b5
lupharos <websenseturkey@gmail.com>
2025-07-27 02:24:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.json(req.headers);  // gelen header'ları JSON olarak döner
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
